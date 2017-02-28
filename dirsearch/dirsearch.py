118640c111048308d0163727c9a5bd169c42b074
Shaun <shaun@healthqtech.com>
2017-02-28 18:41:46 +0200

    First version. Ported from other code.

---
+++ dirsearch/dirsearch.py
@@ -0,0 +1,19 @@
+import os
+import glob
+
+
+def dirsearch(path, extension='*.*', recursive=True):
+    if not os.path.isdir(path):
+        raise ('Error: \'%s\' is not a valid directory', path)
+
+    if recursive:
+        return list([y for x in os.walk(path) for y in _insensitive_glob(os.path.join(x[0], extension))])
+    else:
+        return list(_insensitive_glob(os.path.join(path, extension)))
+
+
+def _insensitive_glob(pattern):
+    def either(c):
+        return '[%s%s]' % (c.lower(), c.upper()) if c.isalpha() else c
+
+    return glob.glob(''.join(map(either, pattern)))
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='dirsearch',
+    packages=['dirsearch'],
+    version='1.0.0',
+    description='A simple wrapper to perform a case-insensitive wildcard directory search',
+    author='Shaun Lodder',
+    author_email='shaun.lodder@gmail.com',
+    url='https://github.com/lodder/python_dirsearch',
+    keywords=['directory search wildcard'],
+    classifiers=[],
+    install_requires=[
+    ],
+    extras_require={
+        'develop': ['nose']
+    }
+)
